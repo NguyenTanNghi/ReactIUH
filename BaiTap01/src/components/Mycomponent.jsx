52bb36995bf3873020aa65c7e00de3470f4a07f1
H82M19\Student <Student>
2025-02-18 17:00:21 +0700

    submit

--- BaiTap01/src/components/Mycomponent.jsx
+++ BaiTap01/src/components/Mycomponent.jsx
@@ -1,19 +1,31 @@
 import React from "react";
+import Childcomponent from "./Childcomponent.jsx";
+import DisplayInfor from "./DisplayInfor.jsx";
 class Mycomponent extends React.Component {
     state = {
-        Name: 'Eric',
-        Age: 28,
-        Address: 'HCM'
+        listUser: [
+            { id: 1, Name: "Dung", Age: 49 },
+            { id: 2, Name: "Hoang", Age: 34 },
+            { id: 3, Name: "Chien", Age: 32 },
+        ]
     }
+
     render() {
         return (
+
             <div>
-                My name is: {this.state.Name}
-                <br />
-                Address: {this.state.Address}
+
+
+                <Childcomponent ></Childcomponent>
+                {/* <UserInfor></UserInfor> */}
+                <hr />
+                <DisplayInfor listUser={this.state.listUser}></DisplayInfor>
+
             </div>
         );
     }
+
+
 }
 
 export default Mycomponent;
