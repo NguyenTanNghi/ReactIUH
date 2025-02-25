cd41859832f126c00c05c0006d151ed6f1a0dc17
H82M19\Student <Student>
2025-02-25 17:08:27 +0700

    commit 25/2

--- TH_Tuan3/Bai01/src/Contact.jsx
+++ TH_Tuan3/Bai01/src/Contact.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 function Contact({ contacts, onDelete }) {
     
-    return (contacts.map((contact) => (
+    return (<>{contacts.map((contact) => (
         <div className="contact" key={contact.id}>
             <div className="info">
                 <h2 className="firstName">{contact.firstName}</h2>
@@ -12,7 +12,7 @@ function Contact({ contacts, onDelete }) {
             <p className="address">Address: <span>{contact.address}</span></p>
             <button className="delete" onClick={() => onDelete(contact.id)}>Delete</button>
         </div>
-    )))
+    ))}</>)
 
 
 
